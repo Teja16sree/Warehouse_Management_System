ddacd36f60a41f32d800e3b76df1c096b41dfc5b
AppMod Upgrade <upgrade@appmod.local>
2026-06-03 17:45:21 +0530

    Created Order entity

---
+++ src/main/java/com/wms/order/Order.java
@@ -0,0 +1,27 @@
+package com.wms.order;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "orders")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Order {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private Integer quantity;
+
+    @Column(nullable = false)
+    private String status;
+
+}
